ab25c9533a0fe0eaf7e5f09f8d2ee614d57449d5
Nhan Nguyen Thanh <nhan2151@gmail.com>
2019-02-21 17:39:58 +0700

    4-DOM-pig-game

--- 3-how-JS-works/script.js
+++ 3-how-JS-works/script.js
@@ -1,30 +1,50 @@
-// calculateAge(1965 );
+// // calculateAge(1965 );
 
-// function calculateAge(year) {
-// 	console.log(2016 - year);
+// // function calculateAge(year) {
+// // 	console.log(2016 - year);
+// // }
+
+
+// // retirement(1956);
+
+// // var retirement = function(year) {
+// // 	console.log(65- (2016 - year));
+// // }
+
+
+// //  First scoping example
+
+// var a = 'Hello!';
+// first();
+
+// function first() {
+// 	var b = 'Hi';
+// 	second();
+
+// 	function second(){
+// 		var c = 'Hey!';
+// 		console.log(a + b + c);
+// 	}
+
 // }
 
 
-// retirement(1956);
 
-// var retirement = function(year) {
-// 	console.log(65- (2016 - year));
-// }
+calculateAge(1985);
 
-
-//  First scoping example
-
-var a = 'Hello!';
-first();
-
-function first() {
-	var b = 'Hi';
-	second();
-
-	function second(){
-		var c = 'Hey!';
-		console.log(a + b + c);
-	}
-
+function calculateAge(year){
+	console.log( 2016 - year );
+	console.log(this);
 }
+
+var join = {
+	name: 'Join',
+	yearOfBirth: 1990,
+	calculateAge: function() {
+		console.log(this);
+	}
+};
+
+join.calculateAge()
+
 
