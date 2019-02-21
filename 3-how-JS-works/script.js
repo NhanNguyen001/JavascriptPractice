e641d7b3377cdd71223959c29f06b45fab51cb60
Nhan Nguyen Thanh <nhan2151@gmail.com>
2019-02-21 16:14:12 +0700

    Learning Scoping and the Scope Chain

--- 3-how-JS-works/script.js
+++ 3-how-JS-works/script.js
@@ -1,12 +1,30 @@
-calculateAge(1965 );
+// calculateAge(1965 );
 
-function calculateAge(year) {
-	console.log(2016 - year);
+// function calculateAge(year) {
+// 	console.log(2016 - year);
+// }
+
+
+// retirement(1956);
+
+// var retirement = function(year) {
+// 	console.log(65- (2016 - year));
+// }
+
+
+//  First scoping example
+
+var a = 'Hello!';
+first();
+
+function first() {
+	var b = 'Hi';
+	second();
+
+	function second(){
+		var c = 'Hey!';
+		console.log(a + b + c);
+	}
+
 }
 
-
-retirement(1956);
-
-var retirement = function(year) {
-	console.log(65- (2016 - year));
-}
