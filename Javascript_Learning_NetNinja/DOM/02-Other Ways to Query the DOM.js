0316f32152cb36cad01f7c0c7f35b3f5891336b3
Nguyen Thanh Nhan <nhan2151@gmail.com>
2020-03-13 13:22:01 +0700

    continue learning javascript DOM with NetNinja

---
+++ Javascript_Learning_NetNinja/DOM/02-Other Ways to Query the DOM.js
@@ -0,0 +1,18 @@
+// get an element by ID
+// const title = document.getElementById("page-title");
+// console.log(title);
+
+// get elements by their class name
+// const errors = document.getElementsByClassName("error");
+// console.log(errors);
+// console.log(errors[0]);
+// console.log(errors[1]);
+// can not use forEach on HTML Collection
+// errors.forEach(error => {
+//   console.log(error);
+// });
+
+// get elements by their tag name
+const paras = document.getElementsByTagName("p");
+console.log(paras);
+console.log(paras[1]);
