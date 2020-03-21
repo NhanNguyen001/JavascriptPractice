0b31ae213b59fda83a1fd799c556c21f77ea78ea
NhanNguyen001 <nhan2151@gmail.com>
2020-03-21 13:16:17 +0700

    complete learning Regular Expressions

--- Learning Regular Expressions/validation.js
+++ Learning Regular Expressions/validation.js
@@ -2,6 +2,30 @@
 // var reg = /[a-z]/i;
 
 // var reg2 = new RegExp(/[a-z]/, 'i');
+
+const input = document.querySelectorAll('input');
+
 const patterns = {
-  telephone: /^\d{11}$/
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{15}$/i,
+  password: /^[\w@-]{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
+
+// function
+function validate(field, regex) {
+  console.log(regex.test(field.value));
+  if (regex.test(field.value)) {
+    field.className = 'valid';
+  } else {
+    field.className = 'invalid';
+  }
+}
+
+input.forEach(input => {
+  input.addEventListener('keyup', e => {
+    // console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
